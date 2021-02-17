bd49b3ac526110298de5a1739f32b38990602090
Yifeng-T <yifeng.tang@duke.edu>
2021-02-17 03:55:43 +0000

    test_egr_analysis.py

--- test_egr_analysis.py
+++ test_egr_analysis.py
@@ -8,18 +8,23 @@ from ehr_analysis import load_labs
 
 
 def test_num_older_than():
-    assert num_older_than(patients, 50) == 77
+    assert num_older_than(
+        patients, 50,
+        ) == 77, "Computated value is incorrect"
 
 def test_load_patients():
-    assert (load_patients('PatientCorePopulatedTable.txt'))[0] == '1947-12-28 02:45:40.547'
+    assert (load_patients
+    ('PatientCorePopulatedTable.txt'))[0] == '1947-12-28 02:45:40.547', "Computated value is incorrect"
 
 def test_get_age():
-    assert (get_age('1952-01-18 19:51:12.917000')) == 69.08
+    assert (get_age('1952-01-18 19:51:12.917000')) == 69.08, "Computated value is incorrect"
 
 def test_sick_patients():
-    assert len(sick_patients(lab_infor, "METABOLIC: ALBUMIN", ">", 4.0)) == 100
+    assert len(sick_patients(
+        lab_infor, "METABOLIC: ALBUMIN", ">", 4.0,
+        )) == 100, "Computated value is incorrect"
 
 def test_load_labs():
-    assert len(load_labs('LabsCorePopulatedTable.txt')) == 3
-    assert len(load_labs('LabsCorePopulatedTable.txt')[0]) == 111483 
+    assert len(load_labs('LabsCorePopulatedTable.txt')) == 3, "input_value has insufficient # of items"
+    assert len(load_labs('LabsCorePopulatedTable.txt')[0]) == 111483, "input value missis some data"
     assert load_labs('LabsCorePopulatedTable.txt')[1][2] == 'CBC: MCH'
