e9143bce5d42c305e2ec89ccf86b76236613726f
Yifeng-T <yifeng.tang@duke.edu>
2021-03-11 17:24:08 +0000

    Yifeng

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -1,182 +1,140 @@
+#------------------------------------
+#for PatientCorePopulatedTable file:
+
 import datetime
 from datetime import datetime
-import pandas as pd
-import matplotlib.pyplot as plt
 
-#read_in data frame
-#sort the date value: in chronological order:
-lab = pd.read_table("./LabsCorePopulatedTable.txt", delimiter = "\t")
-lab['LabDateTime']=pd.to_datetime(lab['LabDateTime'])
-lab.sort_values('LabDateTime',inplace=True)
-#read in patient file:
-pd = pd.read_table("./PatientCorePopulatedTable.txt", delimiter = "\t")
-
-class Patient():
-    """Class Patient"""
-    def __init__(self, ID, sex, birth, race): 
-        """Initialize. 4 instance attributes"""
-        """define the instance attribute for id, where ID is a input value"""
-        #define the date format:
-        date_format = '%Y-%m-%d %H:%M:%S.%f'
-        if not isinstance(ID, str):
-            raise ValueError(f"{ID} is not a string variable")
-        if not isinstance(sex, str):
-            raise ValueError(f"{sex} is not a string variable")
-        if not isinstance(birth, str):
-            raise ValueError(f"{birth} is not a string variable")
-        if not isinstance(race, str):
-            raise ValueError(f"{race} is not a string variable")
-        self.id = ID
-        self.gender = sex
-        self.DOB = datetime.strptime(birth, date_format)
-        self.race = race
-
-        #define the current date:
-        current_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')
-        current_date = datetime.strptime(current_date, date_format)
-        # self.age is a time value
-        total_age = current_date - self.DOB
-        self.age = round(total_age.days/365.25, 2)
+def load_patients(name):
+    # check the input value is a file name
+    if not isinstance(name, str):
+        raise ValueError(f"{name} is not a str file_name: input name should be your file name")
+    pat = [] #set the initial list
+    data_table = {} #set the initial dictionary
+    #read in data with time efficiency O(n^2+n+2)===>O(n^2)
+    with open(name) as stream:
+        data_lines = stream.readlines() #(one assignment dropped)
+        #split the lines at first row
+        pat = data_lines[0].split() #(one assignment dropped)
+        # (n assignment for this loop)
+        for i in pat: 
+            data_table[i]=[] 
+        # (O(n^2) assignment)
+        for j in data_lines[1:]:
+            line_data = j.split('\t')
+            for n in range(0, len(pat)):
+                data_table[pat[n]].append(line_data[n])
+                
     
-    @property
-    def Age(self):
-        """define the property value: age"""
-        # self.age is a time value
-        return self.age
+    return data_table['PatientDateOfBirth']
 
-    def __lt__(self, other):
-        """working comparison operators for less than '<'"""
-        if isinstance(other, Patient): # check other ===? patient class
-            age1 = self.age
-            age2 = other.age
-            if age1 < age2:
-                return "first patient younger than other"
-            else:
-                return "first patient not younger than other"
-        elif isinstance(other, float): # check other ===? float
-            age1 = self.age
-            age2 = other 
-            if age1 < age2:
-                return "first patient younger than other"
-            else:
-                return "first patient not younger than other"
-        else:
-            raise ValueError(f"{other} is not a 'Patient' or float")
-    def __gt__(self, other):
-        """working comparison operators for less than '<'"""
-        if isinstance(other, Patient): # check other ===? patient class
-            age1 = self.age
-            age2 = other.age
-            if age1 > age2:
-                return "first patient older than other"
-            else:
-                return "first patient not older than other"
-        elif isinstance(other, float): # check other ===? float
-            age1 = self.age
-            age2 = other 
-            if age1 > age2:
-                return "first patient older than other"
-            else:
-                return "first patient not older than other"
-        else:
-            raise ValueError(f"{other} is not a 'Patient' or float")
+patients = load_patients('PatientCorePopulatedTable.txt')
 
+
+def num_older_than(patients, age):
+    #check the input patients is a suffieicent list with patiens birth date
+    if not isinstance(patients, list):
+        raise ValueError(f"{patients} is not a sufficient list contains patients birth_date")
+    for i in range(len(patients)):
+        if not isinstance(patients[i], str):
+            raise ValueError(f"{patients[i]} is not a string of birth_date of patient")
+    #check the input age is a sufficient number
+    if not (isinstance(age, float) or isinstance(age, int)):
+        raise ValueError(f"input_value:age {age} is not a sufficient number value")
+    birth = patients
     
+    date_format = '%Y-%m-%d %H:%M:%S.%f'
+    for i in range(0, len(birth)): #change the format: string to date_format
+        birth[i] = datetime.strptime(birth[i], date_format)
     
-    def plot(self, lab_name, picture_name):
-        """plotting the graph: lab values over time"""
-        if not isinstance(lab_name, str):
-            raise ValueError(f"lab_name: {lab_name} should be a string")
-        if not isinstance(picture_name, str):
-            raise ValueError(f"plot_name: {picture_name} should be a string")
-        
-        #----filter the rows have for given patient id
-        is_id = lab["PatientID"] == self.id 
-        lab_select_id = lab[is_id] 
-        #----filter the rows match the input lab_name for given patient
-        is_lab = lab_select_id["LabName"] == lab_name
-        lab_filter = lab_select_id[is_lab]
+    current_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')
+    #change the date_string to date_value
+    current_date = datetime.strptime(current_date, date_format)
 
-        if lab_filter.empty:
-            raise ValueError(f"The patient haven't taken test in {lab_name}")
+    count = 0 #index (recording satisfied number of patients)
+    
+    for i in range(len(birth)):
+        delta = current_date - birth[i] #calculate the date difference
+        year_difference = delta.days/365.25
+        if year_difference > age: 
+         count = count + 1
+    return count
 
-        #-----select the lab value units:
-        Units = lab_filter["LabUnits"]
-        Units = list(Units)
+#-----------------------------------------
+#for LabsCorePopulatedTable file:
+def load_labs(name):
+    if not isinstance(name, str):
+        raise ValueError(f"input_value:file_name {name} is not a sufficient file_name string value")
+    lab = {} # set initial_dictionary
+    keylab = [] # set initial_list
+#read in data: using the dictionary to readin data one line by one line
+# time efficiency (O(n^2))
+    with open(name) as stream:
+        datalines = stream.readlines()
+        keylab = datalines[0].split()
+        for i in keylab:
+            lab[i] = []
+        for j in datalines[1:]:
+            line_data = j.split('\t') #seperate by tab
+            for m in range(len(keylab)):
+                lab[keylab[m]].append(line_data[m])
+    pati_id = lab['\ufeffPatientID']
+#find the lab_name value from dictionary: lab
+    lab_name = lab['LabName']
+#find the test_value from dictionary: lab
+#change the string type to float
+    lab_value = lab['LabValue']
+    for i in range(0, len(lab_value)):
+        lab_value[i] = float(lab_value[i])
+    infor = []
+    infor = [pati_id, lab_name, lab_value]
+    return infor
 
-        #define the X and Y values of plot:        
-        Y = lab_filter["LabValue"]
-        X = lab_filter["LabDateTime"]
-        Y = list(Y)
-        X = list(X)
-        for i in range(0, len(X)):
-            X[i] = str(X[i])
-            X[i] = X[i][0:16]    #date value -> string
-        
-        #clean the previous graph
-        plt.clf()
-        plt.plot(X, Y, marker='o', color = "black")
-        plt.title(f"Patient ID:{self.id} \n Lab Name, {lab_name}",fontsize=12)
-        plt.xlabel("X: Test Time (Year-Month-Date-Hour-Minute)")
-        plt.ylabel(f"Y: Test result unit in {Units[1]}")
-        plt.xticks(rotation = 90)
-        plt.tight_layout()
-        plt.savefig(f"{picture_name}")
-        
-   
-class Observation:
-    """Class Observation"""
-    def __init__(self, ID, lab_value, lab_units): 
-        #lab_value (1.8). lab_unitss ('bc/hpf')
-        """Initialize. 2 instance attributes"""
-        """define the instance attribute for ID, where ID is a input value"""
-        if not isinstance(ID, str):
-            raise ValueError(f"{ID} is not a string variable")
-        if not isinstance(lab_value, float):
-            raise ValueError(f"{lab_value} is not a float variable")
-        if not isinstance(lab_units, str):
-            raise ValueError(f"{lab_units} is not a string variable")
-        self.id = ID
-        self.value = lab_value
-        self.units = lab_units
+lab_infor = load_labs('LabsCorePopulatedTable.txt')
 
-# set patient dictionary: {id : Patient class}
-pdic = {}
-plist = list(pd["PatientID"])
-for i in range(0, len(plist)):
-    pdic[plist[i]] = Patient(pd["PatientID"][i], pd["PatientGender"][i], pd["PatientDateOfBirth"][i], \
-        pd["PatientRace"][i])
+def sick_patients(lab_infor, lab, gt_lt, value): #time efficiency(O(n))
+    if not isinstance(lab_infor, list):
+        raise ValueError(f"input_value:lab_information {lab_infor} is not a sufficient list")
+    if not (isinstance(value, float) or isinstance(value, int)):
+        raise ValueError(f"input_value:value {value} is not a sufficient number value")
+    sick = [] #define an initial list to store sick_patients id
+    pati_id = lab_infor[0]
+    lab_name = lab_infor[1]
+    lab_value = lab_infor[2]
+    for k in range(len(lab_name)):
+        if lab == lab_name[k]:
+            if gt_lt == '>' and lab_value[k] > value:
+                sick.append(pati_id[k])
+            elif gt_lt == '<' and lab_value[k] < value:
+                sick.append(pati_id[k])
+    return set(sick)
 
+def get_age(birth):
+    if not isinstance(birth, str):
+        raise ValueError(f"input_value:birth {birth} is not a sufficient string_of_birthdate")
+    #define the date_format
+    date_format = '%Y-%m-%d %H:%M:%S.%f'
+    #change the birth variable_format string->date_variable
+    birth = datetime.strptime(birth, date_format)
 
-if __name__ == "__main__":
-    """some examples"""
-    patient1 = Patient("1A8791E3-A61C-455A-8DEE-763EB90C9B2C", "Male", \
-        "1947-01-18 19:51:12.917000", "african")
-    patient2 = Patient("220C8D43-1322-4A9D-B890-D426942A3649", "Female", \
-        "1957-01-18 19:51:12.917000", "african")
-    ob1 = Observation("14", 103.3, "mg/dL")
-    #print(patient1.id)
-    #print(patient1.gender)
-    #print(patient1.DOB)
-    #print(patient1.race)
-    #print(ob1.id)
-    #print(ob1.value)
-    #print(ob1.units)
-    patient1.plot("URINALYSIS: RED BLOOD CELLS", "Urbc_overtime.png")
-    Patient("220C8D43-1322-4A9D-B890-D426942A3649", "Female", "1957-01-18 19:51:12.917000", "african").\
-        plot("URINALYSIS: PH", "ph_over_time.png")
-    patient2.plot("URINALYSIS: PH", "Uph_over_time.png")
-    print(patient1 < 57.0) #===> output:first patient not younger than other
-    print(patient1 > patient2) #===> output: first patient older than other
+    #get the current date:
+    current_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')
+    current_date = datetime.strptime(current_date, date_format)
+    #get the date difference
+    day_diff = current_date - birth
+    age = day_diff.days/365.25
 
-    print(pdic['FB2ABB23-C9D0-4D09-8464-49BF0B982F0F'].age) #=>73.2
-    print(pdic['DB92CDC6-FA9B-4492-BC2C-0C588AD78956'].age) #=>43.7
+    #the inpit birth should be a past_date time
+    if age<0:
+        raise ValueError(f"the input_age {birth} should be a past date_time ")
+    
+    age = round(age, 2)
+    return (age)
 
-    print(pdic['FB2ABB23-C9D0-4D09-8464-49BF0B982F0F'] > pdic['DB92CDC6-FA9B-4492-BC2C-0C588AD78956'])
-    #===>first patient older than other
+if __name__ == '__main__':
+    print(num_older_than(patients, 51.2)) #====> 75
+    print(len(sick_patients(lab_infor, "METABOLIC: ALBUMIN", ">", 4.0))) #=====> 100
+    print(get_age("1957-01-18 19:51:12.917000"))
+    print(len(load_labs('LabsCorePopulatedTable.txt')))
+    print(load_labs('LabsCorePopulatedTable.txt')[0][0])
+    print(load_patients('PatientCorePopulatedTable.txt')[0])
 
-    print(pdic['FB2ABB23-C9D0-4D09-8464-49BF0B982F0F'] < 66.0)
-    #===>first patient not younger than other
-
-    pdic['FB2ABB23-C9D0-4D09-8464-49BF0B982F0F'].plot("URINALYSIS: RED BLOOD CELLS", "111.png")
-    print(pdic['DB92CDC6-FA9B-4492-BC2C-0C588AD78956'].gender) #=>Male
