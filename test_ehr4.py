0b6f0239a8e74ddcc2a255825de8fe07e46e93bc
Yifeng-T <yifeng.tang@duke.edu>
2021-03-31 16:49:46 +0000

    Yifeng

--- test_ehr4.py
+++ test_ehr4.py
@@ -8,6 +8,7 @@ def test_patient_sql():
     patient1 = Patient(cur, "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F")
     patient2 = Patient(cur, "220C8D43-1322-4A9D-B890-D426942A3649")
     assert patient1.gender == "Male"
+    assert patient1.race == "Unknown"
     assert patient2.Age == 50.05
 
 def test_observation_sql():
